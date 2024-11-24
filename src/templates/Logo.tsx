0c2960783132af282ae1a02b8261a076b01c5efc
tapeyy <samtape8@gmail.com>
2024-11-24 13:58:48 +1030

    build: sonava content
    Setup of sonava branded content

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -1,23 +1,9 @@
-import { AppConfig } from '@/utils/AppConfig';
+import Image from 'next/image';
 
-export const Logo = (props: {
-  isTextHidden?: boolean;
-}) => (
+import logowhite from '@/public/assets/images/logo-black-on-white.png';
+
+export const Logo = () => (
   <div className="flex items-center text-xl font-semibold">
-    <svg
-      className="mr-1 size-8 stroke-current stroke-2"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M0 0h24v24H0z" stroke="none" />
-      <rect x="3" y="12" width="6" height="8" rx="1" />
-      <rect x="9" y="8" width="6" height="12" rx="1" />
-      <rect x="15" y="4" width="6" height="16" rx="1" />
-      <path d="M4 20h14" />
-    </svg>
-    {!props.isTextHidden && AppConfig.name}
+    <Image src={logowhite} alt="Logo" width={100} height={100} />
   </div>
 );
